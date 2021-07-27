17eb6c0673d901d43e1782fc84a1a90248d60506
charlessoarez <31696186+charlessoarez@users.noreply.github.com>
2021-07-27 01:28:02 -0300

    Update cropCapsuleAndHarvestSystem2.ino

--- cropCapsuleAndHarvestSystem2.ino
+++ cropCapsuleAndHarvestSystem2.ino
@@ -2,22 +2,36 @@
  * Created by Charles Soarez
  * Date: 07-25-2021 
 */
+#include <Servo.h>
+#include <LiquidCrystal.h>
 
 const float OPT_TEMP = 35.0;
-const float OPT_HUM = 500.0;
+const float OPT_HUM = 50.0;
 const float OPT_DIST = 40.0;
 
-#define PIN_HUM 12 //	In the real project, It is used for DTH11.
-#define PIN_ECHO 11
-#define PIN_TRIGGER 10
-#define PIN_BUTTON 9
+#define PIN_TRIGGER 13
+#define PIN_ECHO 12
+#define PIN_BUTTON 11
+#define PIN_RED_LED 10
+#define PIN_GREEN_LED 9
+#define PIN_BLUE_LED 8
+#define PIN_WHITE_LED 7
+#define PIN_TEMP A0
+#define PIN_HUM A1
+#define PIN_SERVO 2
+#define MIN_PULSE 1000
+#define MAX_PULSE 2000
 
 void setup(){
 	Serial.begin(9600);	
-  	pinMode(2, OUTPUT);
   	pinMode(PIN_ECHO, INPUT);
   	pinMode(PIN_TRIGGER, OUTPUT);
   	pinMode(PIN_BUTTON, INPUT);	
+  	pinMode(PIN_RED_LED, OUTPUT);
+  	pinMode(PIN_GREEN_LED, OUTPUT);
+  	pinMode(PIN_BLUE_LED, OUTPUT);
+  	pinMode(PIN_WHITE_LED, OUTPUT);
+  	pinMode(PIN_TEMP, INPUT);
 }
 
 void loop(){
@@ -26,32 +40,76 @@ void loop(){
 		optimalTemperature = false,
   		optimalDistance = false, 
   		selectToCut = false;
-     
-  if(getTemperature() > OPT_TEMP){
-   	optimalTemperature = true; 
-    Serial.println("Optimal temperature: " + (String)(getTemperature()));
-  }else
-  	Serial.println();
-    
+     printToLcd("hola");
   if(getHumidity() > OPT_HUM){
   	optimalHumidity = true;
-    Serial.println("Optimal humidity: " + (String)(getHumidity()));
+    digitalWrite(PIN_RED_LED, HIGH);
+    Serial.println("Optimal humidity: " + (String)(getHumidity()/100));
+  }else{
+ 	digitalWrite(PIN_RED_LED, LOW);
+  	Serial.println("Not an oprimtal humidity: " + (String)(getHumidity()));
   }
+  
+  delay(200);
+  
+  if(getTemperature() > OPT_TEMP){
+   	optimalTemperature = true; 
+    digitalWrite(PIN_GREEN_LED, HIGH);
+    Serial.println("Optimal temperature: " + (String)(getTemperature()));
+    printToLcd("Optimal temperature: " + (String)(getTemperature()));
+  }else{
+    digitalWrite(PIN_GREEN_LED, LOW);
+  	Serial.println("Not an oprimtal temperature: " + (String)(getTemperature()));
+  }
+  
+  delay(200);
   
   if(getDistance() > OPT_DIST){
   	optimalDistance = true;
-    Serial.println("Distance: " + (String)(getDistance()) + " cm");
+    digitalWrite(PIN_BLUE_LED, HIGH);
+    Serial.println("Optimal distance: " + (String)(getDistance()) + " cm");
+  }else{
+  	digitalWrite(PIN_BLUE_LED, LOW);
+    Serial.println("Not optimal Distance: " + (String)(getDistance()) + " cm");
   }
+  
+  delay(200);
   
   if(isButtonPressed()){
   	selectToCut = true;
-    Serial.println("The user wants to harvest: " + (String)(isButtonPressed()));
+    Serial.println("The user pressed the harvest button: " + (String)(isButtonPressed()));
+  }else{
+  	selectToCut = false;
+        digitalWrite(PIN_WHITE_LED, LOW);
   }
-    
-  if(optimalHumidity && optimalTemperature && optimalDistance && selectToCut){
-  	
-  }
+  
   delay(200);
+  
+  if(optimalHumidity && optimalTemperature && optimalDistance && selectToCut){
+  	printToLcd("It is harvest time!");
+    digitalWrite(PIN_WHITE_LED, HIGH);
+    //Here we call the servo to move the ramp
+    elevateServoRamp(1);
+    //Then we wait a litle and close put se servo in the normal state
+    delay(10000);
+    elevateServoRamp(0);
+  }
+  
+  delay(200);
+}
+
+/**
+ * Function that simulates a humidity sensor, probably a DHT11 sensor
+ *@return humidity porcentage
+*/
+float getHumidity(){
+	unsigned int humidity = analogRead(PIN_HUM);
+  	  
+  	delay(500);
+ 
+  	Serial.println(humidity);
+	
+  	return humidity/10;
 }
 
 /**
@@ -59,7 +117,8 @@ void loop(){
  * @return temperature
  */
 float getTemperature(){
-	unsigned int StValue = analogRead(A0);
+	unsigned int StValue = analogRead(PIN_TEMP);
+  	
 	float temperature = 0.0;
   
 	float voltage = (StValue * 5000.0) / 1023.0;
@@ -69,20 +128,6 @@ float getTemperature(){
 	}
 	
   return temperature;
-}
-
-/**
- * Function that simulates a humidity sensor, probably a DHT11 sensor
- *@return humidity porcentage
-*/
-float getHumidity(){
-	unsigned int humidity = analogRead(A1);
-  	  
-  	delay(500);
- 
-  	Serial.println(humidity);
-	
-  	return humidity;
 }
 
 /**
@@ -100,21 +145,56 @@ float getDistance(){
   	digitalWrite(PIN_TRIGGER, LOW);
   
   	duration = pulseIn(PIN_ECHO, HIGH); //Don not recognise this function
-  	distance = (duration / 2) / 29;
+  	distance = (duration / 2.0) / 29.0;
   
-  	if (distance >= 500 || distance <= 0)
+  	Serial.println("distancia: " + (String)(distance));
+  	if (distance >= 500.0 || distance <= 0.0)
   		Serial.println("--");
+      else
+      	Serial.println("distancia: " + (String)(distance));
   
 	return distance;
 }
 
 /*
- *Function that detects if the user wants to harvest the plant
+ * Function that detects if the user wants to harvest the plant
  *@return true or false
  */
 bool isButtonPressed(){
-	if(digitalRead(PIN_BUTTON))
+  bool state = digitalRead(PIN_BUTTON);
+  
+	if(state == 1)
       return true;
      else
       return false;
+}
+
+/**
+ * Method that prints messages in the LCD 
+ */
+void printToLcd(String message){
+	//Creates an LCD object 
+  LiquidCrystal lcd(1, 0, 6, 5, 4, 3);
+  
+  lcd.begin(16, 2);
+  
+  lcd.print("LCD");
+  Serial.println("LCD");
+  delay(1000);
+  //lcd.clear();
+}
+
+/**
+* Funcrion that moves the servo motor from 0° to 180° end vice versa
+*/
+void elevateServoRamp(int option){
+  Servo servo;
+  
+  servo.attach(PIN_SERVO, MIN_PULSE, MAX_PULSE);
+  
+  if(option == 1){
+    servo.write(180);
+  	delay(5000);
+  	servo.write(0);
+  }
 }
